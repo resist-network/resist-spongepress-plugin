d2c21f49fe3f5dabbd684327dc03789b2f5a836c
system-operator <mindamp@gmail.com>
2018-12-30 08:42:27 -0600

    Bleh... automation and stuff.

--- src/main/java/net/resist/wordpress/sponge/PlayerListener.java
+++ src/main/java/net/resist/wordpress/sponge/PlayerListener.java
@@ -47,9 +47,11 @@ public class PlayerListener{
     }
     @Listener
     public void onDamage(DamageEntityEvent event,@Root Player player){
+		try {
         if(Config.blockDamageBeforeLogin){
             event.setCancelled(checkForAccepted(player,Config.mustLoginMsg));
         }
+		} catch (Exception e) {}
     }
     private boolean checkForAccepted(Player player,String message) throws Exception{
         String playerName = player.getName().toString();                
